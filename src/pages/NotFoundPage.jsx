86f6c5c2b08084d9d6a515b49324be994734a125
mikhail <mikhail.waddell@gmail.com>
2024-07-27 13:00:07 -0400

    update not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,7 @@
 import snorlax from '../assets/images/gifs/snorlax.gif';
 import { useNavigate } from 'react-router-dom';
 
-import { Button, Center, Flex, Image, Text } from '@chakra-ui/react';
+import { Button, Center, Image, Text } from '@chakra-ui/react';
 
 function NotFoundPage() {
   const navigate = useNavigate();
@@ -10,19 +10,12 @@ function NotFoundPage() {
   };
   return (
     <>
-      <Flex>
-        <Center marginTop={'300px'}>
-          <Image width={'200px'} src={snorlax} alt="" />
-          <Text
-            paddingLeft={'50px'}
-            textAlign={'center'}
-            justifyContent={'center'}
-            fontSize={'2rem'}
-          >
-            404 Pokemon Not Found
-          </Text>
-        </Center>
-      </Flex>
+      <Center marginTop={'300px'} flexDirection={'column'}>
+        <Image width={'200px'} src={snorlax} alt="" />
+        <Text textAlign={'center'} justifyContent={'center'} fontSize={'2rem'}>
+          404 Pokemon Not Found
+        </Text>
+      </Center>
       <Center>
         <Button onClick={handleClick} marginTop={'50px'}>
           Go Back
