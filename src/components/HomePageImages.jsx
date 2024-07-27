30454a23a21c689302aa4069b8675345c71bea51
mikhail <mikhail.waddell@gmail.com>
2024-07-27 13:08:16 -0400

    update pokeball images

--- src/components/HomePageImages.jsx
+++ src/components/HomePageImages.jsx
@@ -2,15 +2,18 @@ import bulbasaur from '../assets/images/pokemon/bulbasaur.png';
 import pickachu from '../assets/images/pokemon/pikachu.png';
 import charizard from '../assets/images/pokemon/charizard.png';
 import pokedex from '../assets/images/pokemon/pokedex.png';
-import pokeball2 from '../assets/images/pokeballs/pokeball2.png';
-import pokeball3 from '../assets/images/pokeballs/pokeball3.svg';
-import outline2 from '../assets/images/pokeballs/balloutline2.png';
+import pokeball3 from '../assets/images/pokeballs/pokeball3.png';
+import ballOutline2 from '../assets/images/pokeballs/balloutline2.png';
+import ballOutline3 from '../assets/images/pokeballs/balloutline3.svg';
 
 function HomePageImages() {
   return (
     <>
-      <div className="ballOutline">
-        <img src={outline2} alt="pokeball outline" />
+      <div className="ballOutline2">
+        <img src={ballOutline2} alt="pokeball outline" />
+      </div>
+      <div className="ballOutline3">
+        <img src={ballOutline3} alt="pokeball" />
       </div>
       <div className="logo">
         <img src={pokedex} alt="pokedex logo" />
@@ -24,11 +27,8 @@ function HomePageImages() {
       <div className="charizard">
         <img src={charizard} alt="charizard" />
       </div>
-      <div className="pokeball2">
-        <img src={pokeball2} alt="pokeballs" />
-      </div>
       <div className="pokeball3">
-        <img src={pokeball3} alt="pokeball" />
+        <img src={pokeball3} alt="pokeballs" />
       </div>
     </>
   );
