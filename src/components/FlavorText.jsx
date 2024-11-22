44fb35dce700a784efb82de1bab1c0dbec7a0bd3
mikhail <mikhail.waddell@gmail.com>
2024-11-22 09:13:51 -0500

    add numbers to flavor text paginator

--- src/components/FlavorText.jsx
+++ src/components/FlavorText.jsx
@@ -2,63 +2,53 @@ import React, { useState } from 'react';
 import ReactPaginate from 'react-paginate';
 import '../assets/styles/FlavorText.css';
 import { Center, Flex, Text } from '@chakra-ui/react';
-import { AiFillLeftCircle, AiFillRightCircle } from 'react-icons/ai'; // icons form react-icons
-import { IconContext } from 'react-icons'; // for customizing icons
+import { AiFillLeftCircle, AiFillRightCircle } from 'react-icons/ai';
 import '../App.css';
 
-function FlavorText({ textArray }) {
-  // console.log('INSIDE FLAVOR TEXT COMPONENT', textArray);
-  // const [users, setUsers] = useState([...Array(10).keys()].map(i => i + 1));
+const FlavorText = ({ textArray }) => {
   const [pageNumber, setPageNumber] = useState(0);
 
   const linesPerPage = 1;
-  const pagesVisited = pageNumber * linesPerPage;
-
-  const displayText = textArray
-    .slice(pagesVisited, pagesVisited + linesPerPage)
-    .map(flavorText => {
-      return (
-        <>
-          <div className="flavorText">
-            <p key={flavorText}>{flavorText}</p>
-          </div>
-        </>
-      );
-    });
-
   const pageCount = Math.ceil(textArray.length / linesPerPage);
 
-  const changePage = ({ selected }) => {
+  const handlePageChange = ({ selected }) => {
     setPageNumber(selected);
   };
 
+  const currentText = textArray.slice(
+    pageNumber * linesPerPage,
+    (pageNumber + 1) * linesPerPage
+  );
+
   return (
-    <Center flexDirection={'column'} margin={'20px'}>
+    <Center flexDirection="column" margin="20px">
       <Text
         className="text"
-        marginTop={'40px'}
-        fontFamily={'Alleyn W01 Regular'}
+        mt="40px"
+        fontFamily="Alleyn W01 Regular"
+        textAlign="center"
       >
-        {displayText}
+        {currentText.map((flavorText, index) => (
+          <div className="flavorText" key={index}>
+            {flavorText}
+          </div>
+        ))}
       </Text>
-      <Flex margin={'0px'}>
+      <Flex mt="10px">
         <ReactPaginate
           previousLabel={'prev'}
           nextLabel={'next'}
           pageCount={pageCount}
-          onPageChange={changePage}
-          pageRangeDisplayed={0}
-          marginPagesDisplayed={0}
-          containerClassName={'paginationBttns'}
-          previousLinkClassName={'previousBttn'}
-          nextLinkClassName={'nextBttn'}
-          disabledClassName={'paginationDisabled'}
-          activeClassName={'paginationActive'}
-          
+          onPageChange={handlePageChange}
+          containerClassName="paginationBttns"
+          previousLinkClassName="previousBttn"
+          nextLinkClassName="nextBttn"
+          disabledClassName="paginationDisabled"
+          activeClassName="paginationActive"
         />
       </Flex>
     </Center>
   );
-}
+};
 
 export default FlavorText;
--- src/components/MainPokemonTab.jsx
+++ src/components/MainPokemonTab.jsx
@@ -1,14 +1,13 @@
 import Loading from './Loading';
 import MainPokemonName from './MainPokemonName';
 import InfoTab from './InfoTab';
-import PokemonCard from './PokemonCard';
+import React, { Suspense } from 'react';
 import ball from '../assets/images/pokeballs/pokeball.png';
 import { CgPokemon } from 'react-icons/cg';
 import { MdGif } from 'react-icons/md';
 import { FaInfo } from 'react-icons/fa';
 import groupImg from '../assets/images/pokeballs/group.png';
-import { useEffect, useState, useRef } from 'react';
-import { useOutletContext, useParams } from 'react-router-dom';
+import { useOutletContext } from 'react-router-dom';
 import { getType } from '../utils';
 import {
   Tabs,
@@ -21,138 +20,104 @@ import {
   Flex,
 } from '@chakra-ui/react';
 
+const LazyPokemonCard = React.lazy(() => import('./PokemonCard'));
+
 function MainPokemonTab() {
   const { team, myTeam, setMyTeam, disabled, setDisabled, isLoading, pokemon } =
     useOutletContext();
+
   return (
     <>
       {isLoading ? (
         <Loading />
       ) : (
-        <Tabs
-          align="center"
-          variant="enclosed"
-          size="lg"
-          maxHeight={'100px'}
-          // outline={'2px solid white'}
-        >
-          {isLoading ? (
-            <Loading />
-          ) : (
-            <TabPanels height={'650px'}>
-              <TabPanel>
-                {/* Main Image */}
-                <MainPokemonName />
-
-                <Center
-                  objectFit="contain"
-                  height={'550px'}
-                  flexDirection={'column'}
-                >
-                  <Image
-                    maxW={'90%'}
-                    maxH={'90%'}
-                    src={pokemon.sprites.other.home.front_default}
-                  />
-                </Center>
-              </TabPanel>
-              <TabPanel>
-                {/* Art Image */}
-                <MainPokemonName />
-                <Center objectFit="contain" height={'550px'}>
-                  <Image
-                    maxW={'90%'}
-                    maxH={'90%'}
-                    src={
-                      pokemon.sprites.other[`official-artwork`].front_default
-                    }
-                  />
-                </Center>
-              </TabPanel>
-              <TabPanel>
-                {/* GIF Image */}
-                <MainPokemonName />
-                <Center objectFit="contain" height={'550px'} flexDir={'column'}>
-                  <Image
-                    minWidth={'150px'}
-                    src={
-                      pokemon.sprites.other.showdown.front_default == null
-                        ? ball
-                        : pokemon.sprites.other.showdown.front_default
-                    }
-                  />
-                </Center>
-              </TabPanel>
-              <TabPanel>
-                {/* INFO TAB */}
-                <Center objectFit="contain" minHeight={'550px'}>
-                  <InfoTab />
-                </Center>
-              </TabPanel>
-              <TabPanel>
-                {/* <h1>Team Tab</h1>
-                {team.map((card, idx) => {
-                  console.log('Card:', idx, card);
-                  return (
-                    <PokemonCard
-                      key={idx}
-                      index={idx}
-                      card={card}
-                      src={card.sprites.other[`official-artwork`].front_default}
-                      src2={card.sprites.other.showdown.front_default}
-                      name={card.name}
-                      pokemon={card}
-                      type={getType(card.types)}
-                      id={card.id}
-                      isLoading={isLoading}
-                      team={team}
-                      setTeam={setTeam}
-                      disabled={disabled}
-                      setDisabldisableded={setDisabled}
-                    />
-                  );
-                })} */}
-                <MainPokemonName pokemonName={pokemon.name} isTeam={'true'} />
-                <Flex
-                  flexWrap="wrap"
-                  paddingTop={'60px'}
-                  justifyContent="center"
-                  maxW={'100vw'}
-                  height={'550px'}
-                  gap="25px"
-                  overflow={'scroll'}
-                >
-                  {isLoading ? (
-                    <Loading />
-                  ) : (
-                    team.map((card, idx) => {
-                      // console.log('Card:', idx, card);
-                      return (
-                        <PokemonCard
-                          key={idx}
-                          index={idx}
-                          card={card}
-                          src={
-                            card.sprites.other[`official-artwork`].front_default
-                          }
-                          src2={card.sprites.other.showdown.front_default}
-                          name={card.name}
-                          pokemon={pokemon}
-                          type={getType(card.types)}
-                          id={card.id}
-                          isLoading={isLoading}
-                          team={team}
-                          disabled={disabled}
-                          setDisabled={setDisabled}
-                          myTeam={myTeam}
-                        />
-                      );
-                    })
-                  )}
-                </Flex>
-              </TabPanel>
-            </TabPanels>
-          )}
+        <Tabs align="center" variant="enclosed" size="lg" maxHeight={'100px'}>
+          <TabPanels height={'650px'}>
+            <TabPanel>
+              {/* Main Image */}
+              <MainPokemonName />
+              <Center
+                objectFit="contain"
+                height={'550px'}
+                flexDirection={'column'}
+              >
+                <Image
+                  maxW={'90%'}
+                  maxH={'90%'}
+                  src={pokemon.sprites.other.home.front_default}
+                />
+              </Center>
+            </TabPanel>
+            <TabPanel>
+              {/* Art Image */}
+              <MainPokemonName />
+              <Center objectFit="contain" height={'550px'}>
+                <Image
+                  maxW={'90%'}
+                  maxH={'90%'}
+                  src={pokemon.sprites.other[`official-artwork`].front_default}
+                />
+              </Center>
+            </TabPanel>
+            <TabPanel>
+              {/* GIF Image */}
+              <MainPokemonName />
+              <Center objectFit="contain" height={'550px'} flexDir={'column'}>
+                <Image
+                  minWidth={'150px'}
+                  src={
+                    pokemon.sprites.other.showdown.front_default == null
+                      ? ball
+                      : pokemon.sprites.other.showdown.front_default
+                  }
+                />
+              </Center>
+            </TabPanel>
+            <TabPanel>
+              {/* INFO TAB */}
+              <Center objectFit="contain" minHeight={'550px'}>
+                <InfoTab />
+              </Center>
+            </TabPanel>
+            <TabPanel>
+              <MainPokemonName pokemonName={pokemon.name} isTeam={'true'} />
+              <Flex
+                flexWrap="wrap"
+                paddingTop={'60px'}
+                justifyContent="center"
+                maxW={'100vw'}
+                height={'550px'}
+                gap="25px"
+                overflow={'scroll'}
+              >
+                {isLoading ? (
+                  <Loading />
+                ) : (
+                  team.map((card, idx) => (
+                    <Suspense key={idx} fallback={<Loading />}>
+                      <LazyPokemonCard
+                        index={idx}
+                        card={card}
+                        src={
+                          card.sprites.other[`official-artwork`].front_default
+                        }
+                        src2={card.sprites.other.showdown.front_default}
+                        name={card.name}
+                        pokemon={pokemon}
+                        type={getType(card.types)}
+                        id={card.id}
+                        isLoading={isLoading}
+                        team={team}
+                        disabled={disabled}
+                        setDisabled={setDisabled}
+                        myTeam={myTeam}
+                      />
+                    </Suspense>
+                  ))
+                )}
+              </Flex>
+            </TabPanel>
+          </TabPanels>
           <TabList>
             <Tab>
               <CgPokemon color="#ef5350" size={'2.5em'} />
