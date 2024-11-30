007fe301fa65f0e27e1fbba753193074846c7366
mikhail <mikhail.waddell@gmail.com>
2024-11-30 09:57:16 -0500

    update

--- src/components/PokemonCard/FlavorText.jsx
+++ src/components/PokemonCard/FlavorText.jsx
@@ -6,35 +6,24 @@ import { Center, Flex, Text, Box, useMediaQuery } from '@chakra-ui/react';
 const FlavorText = ({ flavorTextArray }) => {
   const [pageNumber, setPageNumber] = useState(0);
   const [isMobile] = useMediaQuery('(max-width: 900px)'); // Media query for mobile mode
-  console.log('NEW:', flavorTextArray);
 
   const linesPerPage = 1;
 
-  // Filter for English flavor text and remove duplicates
-  const filteredTextArray = Array.from(
-    new Set(
-      flavorTextArray
-        .filter(item => item.language?.name === 'en') // Check if item.language exists
-        .map(item => item.flavor_text)
-    )
-  ).map(uniqueFlavorText => {
-    return flavorTextArray.find(item => item.flavor_text === uniqueFlavorText);
-  });
-
-  const pageCount = Math.ceil(filteredTextArray.length / linesPerPage);
+  const pageCount = Math.ceil(flavorTextArray.length / linesPerPage);
 
   const handlePageChange = ({ selected }) => {
     setPageNumber(selected);
   };
 
-  const currentText = filteredTextArray.slice(
+  const currentText = flavorTextArray.slice(
     pageNumber * linesPerPage,
     (pageNumber + 1) * linesPerPage
   );
-  console.log('CURRENT TEXT:', currentText);
+
+  console.log('Test 1:', currentText);
 
   return (
-    <Center flexDirection="column" mt="20px" maxW="90%" outline={'2px solid'}>
+    <Center flexDirection="column" mt="20px" maxW="90%">
       <Text
         margin={{ base: '30px', md: '40px' }}
         fontFamily="Alleyn W01 Regular"
@@ -45,11 +34,7 @@ const FlavorText = ({ flavorTextArray }) => {
         fontSize={{ base: '17px', md: '20px' }}
         lineHeight={{ base: '20px', md: '24px' }}
       >
-        {currentText.map((flavorText, index) => (
-          <span className="flavorText" key={index}>
-            {flavorText.flavor_text || 'No text available'}
-          </span>
-        ))}
+        <span className="flavorText">{currentText}</span>
       </Text>
 
       <Center mt={{ base: '10px', md: '20px' }} width="100%">
